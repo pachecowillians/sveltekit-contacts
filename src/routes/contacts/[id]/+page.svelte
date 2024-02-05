0d872ce1f7b613a23440d4bb5d3ec0f5a182a2fa
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-02-05 08:50:44 -0300

    feat: creatinig contact page

--- src/routes/contacts/[id]/+page.svelte
+++ src/routes/contacts/[id]/+page.svelte
@@ -1,6 +1,45 @@
-<script></script>
+<script>
+	import Button from '$lib/button.svelte';
+</script>
 
-<main></main>
+<main>
+	<div class="profile-picture">
+		<img src="/contact.png" alt="Imagem Centralizada" />
+	</div>
+	<div class="profile-name">Ethelyn Ferreras</div>
+	<div class="profile-email">sroots85@wunderground.com</div>
+	<Button img_src="/call.svg" />
+</main>
 
 <style>
+	main {
+		text-align: center;
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		flex: 1;
+		justify-content: center;
+		transform: translateY(-4rem);
+	}
+	.profile-picture {
+		border-radius: 50%;
+		width: 12rem;
+		margin: 0 auto;
+		overflow: hidden;
+	}
+	img {
+		width: 100%;
+	}
+	.profile-name {
+		font-size: 2rem;
+		font-weight: bold;
+		margin-top: 3rem;
+	}
+	.profile-email {
+		font-size: 1rem;
+		color: #777777;
+		margin-top: 0.5rem;
+		margin-bottom: 3rem;
+	}
 </style>
