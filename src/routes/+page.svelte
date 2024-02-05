7765b8b6d88f0d445a9a3cfef47e3cd7fd11380b
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-02-04 23:17:24 -0300

    feat: creating homepage structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script></script>
+
+<div class="content-wrapper">
+	<div class="image-container">
+		<img src="favicon.png" alt="Imagem Centralizada" />
+	</div>
+
+	<div class="button-container">
+		<button>Clique Aqui</button>
+	</div>
+</div>
+
+<style>
+	.content-wrapper {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.image-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-grow: 1;
+	}
+
+	img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+
+	.button-container {
+		margin-bottom: 10vh;
+		text-align: center;
+	}
+
+	button {
+		padding: 10px 20px;
+		background-color: #0074d9;
+		color: #ffffff;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+</style>
